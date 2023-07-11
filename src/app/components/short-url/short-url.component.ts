8f9a45aef3d2c45cbfec4123cf393703d7c9a944
[Rvjonh] <[jonhvelasco3@gmail.com]>
2023-07-11 10:56:59 -0400

    feat: manage errors in short-urls

--- src/app/components/short-url/short-url.component.ts
+++ src/app/components/short-url/short-url.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { ShortUrlService } from 'src/app/services/short-url.service';
 
 @Component({
@@ -18,27 +18,34 @@ export class ShortUrlComponent {
 
   handleCreateUrl(){
     const urlTester = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+    this.isUrlProcessed = false;
     
     if(!urlTester.test(this.url)){
-      this.showErrorMessage("Plesae enter a valid url");
+      this.showErrorMessage("Please enter a valid url");
       return;
     }
     
-    this.isUrlProcessed = false;
     this.processingUrl = true;
 
     this._shortUrlService.getUrlProcessed(this.url).subscribe((data)=>{
       this.urlProcessed = data.link;
       this.isUrlProcessed = true;
       this.processingUrl = false;
+    }, 
+    (error)=>{
+      this.showErrorMessage("We couldn't process this url, check it or try another.", 0);
+      this.isUrlProcessed = false;
+      this.processingUrl = false;
     })
   }
 
   showErrorMessage(message:string, hideTime=3000){
     this.errorMessage = message;
-    setTimeout(()=>{
-      this.errorMessage = ''
-    }, hideTime)
+    if(hideTime > 0){
+      setTimeout(()=>{
+        this.errorMessage = ''
+      }, hideTime)
+    }
   }
 
   hideErrorMessage(){
