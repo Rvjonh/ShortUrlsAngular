f7d63a9f5b300053a70f71f3a5ca3fa60ec4d5bd
[Rvjonh] <[jonhvelasco3@gmail.com]>
2023-07-11 11:07:34 -0400

    feat: interceptor http

--- src/app/services/short-url.service.ts
+++ src/app/services/short-url.service.ts
@@ -8,15 +8,13 @@ import { Observable } from 'rxjs';
 export class ShortUrlService {
 
   URL = 'https://api-ssl.bitly.com/v4/shorten';
-  token = 'a5c9a02a39e0093258f49d50218eaa8b16825aa1';
 
   constructor(private _http: HttpClient) {}
 
   getUrlProcessed(url:string): Observable<any>{
-    const header = new HttpHeaders({Authorization: `Bearer ${this.token}`})
     const body =  {
       "long_url": url,
     } 
-    return this._http.post(this.URL, body, { headers: header});
+    return this._http.post(this.URL, body);
   }
 }
